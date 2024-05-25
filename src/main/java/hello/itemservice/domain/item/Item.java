965b0deb06f11a4e8c74e5bbb1276f98a0e89d67
wkdckd41 <wkdckd41@gmail.com>
2024-05-25 23:50:05 +0900

    타임리프 - 스프링 통합과 폼 - 요구사항 추가

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -10,6 +10,11 @@ public class Item {
     private Integer price;
     private Integer quantity;
 
+    private Boolean open; // 판매 여부
+    private String region; // 등록 지역
+    private ItemType itemType; // 상품 종류
+    private String deliveryCode; // 배송 방식
+
     public Item() {
     }
 
