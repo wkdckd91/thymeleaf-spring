780e77d3437ea9339847137f865963584a7490d5
wkdckd41 <wkdckd41@gmail.com>
2024-05-27 21:43:47 +0900

    타임리프 - 스프링 통합과 폼 - 체크박스 - 멀티

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,6 +2,8 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Item {
 
@@ -11,7 +13,7 @@ public class Item {
     private Integer quantity;
 
     private Boolean open; // 판매 여부
-    private String region; // 등록 지역
+    private List<String> regions; // 등록 지역
     private ItemType itemType; // 상품 종류
     private String deliveryCode; // 배송 방식
 
--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -33,7 +33,7 @@ public class ItemRepository {
         findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
         findItem.setOpen(updateParam.getOpen());
-        findItem.setRegion(updateParam.getRegion());
+        findItem.setRegions(updateParam.getRegions());
         findItem.setItemType(updateParam.getItemType());
         findItem.setDeliveryCode(updateParam.getDeliveryCode());
     }
